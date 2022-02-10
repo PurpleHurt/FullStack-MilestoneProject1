213532367afd90e9f97b6f24f588b3a08f702e8f
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-02-09 18:36:13 -0600

    fixed broken code

--- HotDice/js/play.js
+++ HotDice/js/play.js
@@ -4,15 +4,15 @@ console.log(localStorage.playerArray)
 function poplulatePlayers() {
     var playerNames = JSON.parse(localStorage.getItem("playerArray"))
     console.log(playerNames)
-    var thead = document.createElement('head');
-    for (var i = 0; i < playerArray.length; i++) {
-        thead.appendChild(document.createElement("th"))
 
-    }
 }
 
 
 
 poplulatePlayers()
 
-//appendChilde(document.createTextNode(playerArray[i]));
+//var thead = document.createElement('head');
+//for (var i = 0; i < playerArray.length; i++) {
+//    thead.appendChild(document.createElement("th"))
+//    appendChild(document.createTextNode(playerArray[i]));
+//}
